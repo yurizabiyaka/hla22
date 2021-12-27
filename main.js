b2b7a13cde507961c864bbc3f87031a6cab8a9a5
Yuri Zabiyaka <yzabiyaka@ozon.ru>
2021-12-27 11:51:34 +0300

    Local components registration and two-way data flow

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
 // Main.js
-export default {
+import News from './News.js'
 
+export default {
     data() {
       return {
         message: 'You Loaded this page on ' + new Date().toLocaleString()
       }
     },
-    template: `<button> {{ message }}  </button> <p> <button-counter></button-counter>`
+
+    components: {
+      'news': News
+    },
+
+    template: `<div>
+    <button> {{ message }}  </button>
+    <p> <button-counter> </button-counter> </p>
+    <p> <news></news> </p>
+    </div>
+    `,
 }
-  
