c324908e0b9b1a1177b66227fb7b564a461d0035
Yuri Zabiyaka <yzabiyaka@ozon.ru>
2022-01-29 19:17:20 +0300

    added backend address in config file

--- frontend/config.js
+++ frontend/config.js
@@ -4,6 +4,6 @@ import cfg from "./config_env_file.js"
 export default {
     backendUrl: () => {
         //FRONTEND_BACKEND_PROTOCOL + "://" + LISTEN_HOSTANDPORT + "/v1"
-        return cfg.FRONTEND_BACKEND_PROTOCOL+ "://" + cfg.LISTEN_HOSTANDPORT+ "/v1"
+        return cfg.FRONTEND_BACKEND_PROTOCOL+ "://" + cfg.FRONTEND_BACKEND_HOSTANDPORT+ "/v1"
     }
 }
